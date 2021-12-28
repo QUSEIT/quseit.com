c14e5f7a55153341ba3b06979e086f594b1545aa
莫文凤 <mia_0513@163.com>
2021-12-28 18:27:52 +0800

    refactor: 修改首页官网

--- themes/quseit/source/static/main.js
+++ themes/quseit/source/static/main.js
@@ -1,5 +1,23 @@
 var header = document.querySelector(".q-header");
+const $ = jQuery
 
 document.querySelector(".head-toggle").onclick = function () {
   header.classList.toggle("active");
 }
+
+$(document).ready(function(){
+  $('.your-class').slick({
+      infinite = true,
+      slidesToShow =  1,
+      slidesToScroll = 1,
+      autoplay = true,
+      autoplaySpeed = 2000,
+  }
+  
+    // infinite = true,
+    // slidesToShow =  1,
+    // slidesToScroll = 1,
+    // autoplay = true,
+    // autoplaySpeed = 2000,
+  );
+})
