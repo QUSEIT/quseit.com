00762564785893ab837f70a5982a441cc847e631
谢嘉明 <administrator@xiejiamingdeMacBook-Pro.local>
2022-01-21 17:14:38 +0800

    feat(): 国际化

--- static/main.js
+++ static/main.js
@@ -1,5 +1,34 @@
-var header = document.querySelector(".q-header");
+window.addEventListener('scroll', function () {
+  let tou = document.querySelector('.q-header');
+  tou.classList.toggle("q-header-change", window.scrollY > 0);
+})
 
-document.querySelector(".head-toggle").onclick = function () {
-  header.classList.toggle("active");
-}
+jQuery('.single-item').slick({
+  dots: true,
+  arrows: false,
+  fade: true,
+  cssEase: 'linear',
+  autoplay: true,
+  autoplaySpeed: 10000
+});
+
+jQuery("#changeLang").mouseover(function () {
+  const ul = document.querySelector('#changeLangUl');
+  if (ul) {
+    jQuery("#changeLangBox").children().show();
+  } else {
+    jQuery("#changeLangBox").append('<ul id="changeLangUl"><li id="en"><span>English</span></li><li id="zh"><span>Chinese</span></li></ul>');
+    jQuery("#changeLangBox ul li").click(function () {
+      window.location.pathname = `en${window.location.pathname}`
+    })
+  }
+});
+jQuery("#changeLang").mouseout(function () {
+  jQuery("#changeLangBox").children().hide();
+});
+jQuery("#changeLangBox").mouseover(function () {
+  jQuery("#changeLangBox").children().show();
+});
+jQuery("#changeLangBox").mouseout(function () {
+  jQuery("#changeLangBox").children().hide();
+});
